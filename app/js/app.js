98bfc62e9046da66baa9b35f9efefb7660edb475
huynguyenptit <huynguyenptit97@gmail.com>
2017-05-28 17:26:07 +0700

    add introduce site

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+// $(window).on('scroll', function () {
+//     var scroll_height = $('body').scrollTop();
+//     if(scroll_height > 100){
+//         $('.contact_fixed').attr('style','display:block');
+//     }
+//     else $('.contact_fixed').attr('style','display:none');
+// });
